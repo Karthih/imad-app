63e36845b5acb304eec58a5b363a124cecb0c83b
Karthih <36565366+Karthih@users.noreply.github.com>
2018-02-21 21:43:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,27 +6,27 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('Article two requested and will be served here.');
+ res.send('Article two requested and will be served here.');
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('Article three requested and will be served here.');
+ res.send('Article three requested and will be served here.');
 });
 
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+ res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+ res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
