9bb0815d8f1ffbee02df39c5f033f97c51fa0be5
Karthih <36565366+Karthih@users.noreply.github.com>
2018-02-22 19:33:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,8 +94,6 @@ app.get('/', function (req, res) {
 //articleName == article-one
 //articles[articleName] == {} content object for article-one
 
-articleName = req.params.articleName;
-
 app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
