981905fc89190f4e62561bb0fc47226ce9b1687b
Karthih <36565366+Karthih@users.noreply.github.com>
2018-02-28 19:18:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,25 @@
 console.log('Loaded!');
+
+var element = document.getElementById('main-text');
+element.innerHTML = `Hi! I am H. Karthi, the totally cool person.
+I just created a new app! Yesss!`;
+
+var img = img.getElementById('madi');
+
+var mL = 0;
+var mR = 0;
+var mB = 0;
+var mT = 0;
+
+function move () {
+    mL = mL+10;
+    img.style.mL = mL + 'px';
+    mB = mB+10;
+    img.style.mB = mB + 'px';
+    mR = mR+20;
+    img.style.mR = mR + 'px';
+    mT = mT+10;
+    img.style.mT = mT + 'px';
+    mL = mL+10;
+    img.style.mL = mL + 'px';
+}
