93fc77960db2e86f1a1d9965602c22f81cdc04e1
Karthih <36565366+Karthih@users.noreply.github.com>
2018-02-28 19:19:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 
 var element = document.getElementById('main-text');
-element.innerHTML = `Hi! I am H. Karthi, the totally cool person.<br>
-I just created a new app! Yesss!`;
+element.innerHTML = `Hi! I am H. Karthi, the totally cool person.`<br>
+`I just created a new app! Yesss!`;
 
 var img = img.getElementById('madi');
 
