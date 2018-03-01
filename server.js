8f6d2b1424451568681c137214e18e1cdb14135d
Karthih <36565366+Karthih@users.noreply.github.com>
2018-03-01 19:55:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,10 +97,11 @@ app.get('/', function (req, res) {
 });
 
 var counter = 0;
-app.get('/counter', function (req,res) {
+
+app.get('/counter', function (req, res) {
     counter = counter + 1;
-    res.send(counter, toString());
-    });
+    res.send(counter);
+});
 
 //articleName == article-one
 //articles[articleName] == {} content object for article-one
