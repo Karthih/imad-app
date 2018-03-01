e96c04bf8abc317d433113e40f51f16b943696fa
Karthih <36565366+Karthih@users.noreply.github.com>
2018-03-01 19:56:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ var counter = 0;
 
 app.get('/counter', function (req, res) {
     counter = counter + 1;
-    res.send(counter);
+    res.send(counter, toString());
 });
 
 //articleName == article-one
