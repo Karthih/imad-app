94311384ddb1ecdd4cb54c2e1033eb812823f277
Karthih <36565366+Karthih@users.noreply.github.com>
2018-03-01 20:08:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,11 +92,13 @@ var HTMLTemplate =
 return HTMLTemplate;
 }
 
-var counter = 0;
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    var counter = 0;
+    app.get('/counter', function (req, res) {
     counter = counter + 1;
-    res.send(counter.toString());
+    res.send(counter.toStrin());
+    });
 });
 
 //articleName == article-one
