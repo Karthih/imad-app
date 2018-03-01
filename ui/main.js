27c7d873d0c5edada676bfdbddda2e0fa690dbed
Karthih <36565366+Karthih@users.noreply.github.com>
2018-03-01 18:55:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,26 +3,26 @@ console.log('Loaded!');
 var img = document.getElementById('madi');
 
 var marginLeft = 0;
-var mR = 0;
-var mB = 0;
-var mT = 0;
+var marginRight = 0;
+var marginBottom = 0;
+var marginTop = 0;
 
 function move () {
-    mL = mL+10;
-    img.style.mL = mL + 'px';
-    mB = mB+10;
-    img.style.mB = mB + 'px';
-    mR = mR+20;
-    img.style.mR = mR + 'px';
-    mT = mT+10;
-    img.style.mT = mT + 'px';
-    mL = mL+10;
-    img.style.mL = mL + 'px';
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+    marginBottom = marginBottom + 100;
+    img.style.marginBottom = mB + 'px';
+    marginRight = marginRight + 200;
+    img.style.marginRight = marginRight + 'px';
+    marginTop = marginTop + 100;
+    img.style.marginTop = marginTop + 'px';
+    marginLeft = marginLeft + 100;
+    img.style.marginLeft = marginLeft + 'px';
 }
 function moveRight () {
     marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(move, 1000);
 };
