453028dd1c072d99c90d356adaa400017ff4f39b
Karthih <36565366+Karthih@users.noreply.github.com>
2018-03-01 20:25:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,11 +98,11 @@ app.get('/', function (req, res) {
 
 var counter = 0;
 app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
     res.send(`<html>
               <a href = "http://umah15.imad.hasura-app.io/">Home</a>
               </html>`);
-              counter = counter + 1;
-    res.send(counter.toString());
 });
 
 //articleName == article-one
