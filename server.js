7c50199745925095c17f9fd97261337cb4c145f3
Karthih <36565366+Karthih@users.noreply.github.com>
2018-03-01 19:50:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,8 +93,8 @@ return HTMLTemplate;
 }
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-  var counter = 0;
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    var counter = 0;
     app.get('/counter', function (req,res) {
         counter = counter + 1;
         res.send(counter,toString());
