32bb0b69405ec9b990879f56c5ed488ec1115260
Karthih <36565366+Karthih@users.noreply.github.com>
2018-05-11 21:07:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,4 @@
-//console.log('Loaded!');
 
-//var img = document.getElementById('madi');
-
-//var marginLeft = 0;
-//var marginRight = 0;
-//var marginBottom = 0;
-//var marginTop = 0;
-
-//function move () {
-    //marginLeft = marginLeft + 10;
-    //img.style.marginLeft = marginLeft + 'px';
-    //marginBottom = marginBottom + 10;
-    //img.style.marginBottom = mB + 'px';
-    //marginRight = marginRight + 20;
-    //img.style.marginRight = marginRight + 'px';
-    //marginTop = marginTop + 10;
-    //img.style.marginTop = marginTop + 'px';
-    //marginLeft = marginLeft + 10;
-    //img.style.marginLeft = marginLeft + 'px';
-//}
-//function moveRight () {
-    //marginLeft = marginLeft + 10;
-    //img.style.marginLeft = marginLeft + 'px';
-//}
-//img.onclick = function () {
-    //var interval = setInterval(move, 100);
-//};
 
 var button = document.getElementbyId('counter');
 var counter = 0;
